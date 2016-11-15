66dd2b40ca022c1e10a6639bd4936e238d2513b6
Sergey Radygin <serzh_radygin@mail.ru>
2016-11-15 16:57:15 +0300

    Редактирование анимации и не только
    Настроил позиционирование формы поиска отелей спомощью translateX(-50%).
    Исправленна анимация формы, тепрь форма выезжает и встает сразу на свое
    место.
    Добавлен класс для <html> no-js - для прогрессивного улучшения (при
    выключенном js форма остается видна)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+
+var html = document.documentElement;
 var link = document.querySelector(".search_push");
 var popup = document.querySelector(".search_form");
-			
+
+
+
+html.className = html.className.replace("no_js","js");	
+
 link.addEventListener("click", function(event) {
 event.preventDefault();
 popup.classList.toggle("search_form_show");
